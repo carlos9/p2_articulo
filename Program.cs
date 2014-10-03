0c48939b6d7b5be1d26f6741df0dd236a272d32d
carlos9 <carlooz.ulloa@hotmail.com>
2014-10-03 11:41:13 -0500

    base del programa

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Choche
+ * Date: 03/10/2014
+ * Time: 11:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace p2_articulo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
