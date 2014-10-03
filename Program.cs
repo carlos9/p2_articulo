f44e1b6ee94216cb332b2429c4b307a649b071ad
carlos9 <carlooz.ulloa@hotmail.com>
2014-10-03 12:07:02 -0500

    codigo terminado

--- Program.cs
+++ Program.cs
@@ -14,12 +14,45 @@ namespace p2_articulo
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("Dame la cantidad del Producto 1:");
+			int pro1 = int.Parse( Console.ReadLine());
+			Console.WriteLine("Dame la Descripcion del producto 1:");
+			string des1 = Console.ReadLine();
+			Console.WriteLine("Dame el Precio Unitario del producto 1:");
+			int  pre1 = int.Parse( Console.ReadLine());
+			Console.WriteLine("Dame la cantidad del Producto 2:");
+			int pro2 = int.Parse( Console.ReadLine());
+			Console.WriteLine("Dame la Descripcion 2:");
+			string des2 = Console.ReadLine();
+			Console.WriteLine("Dame el Precio Unitario 2:");
+			int  pre2 = int.Parse( Console.ReadLine());
+			Console.WriteLine("Dame la cantidad del Producto 3:");
+			int pro3= int.Parse( Console.ReadLine());
+			Console.WriteLine("Dame la Descripcion 3:");
+			string des3= Console.ReadLine();
+			Console.WriteLine("Dame el Precio Unitario 3:");
+			int  pre3= int.Parse( Console.ReadLine());
+			int resul1 = pre1*pro1;
+			int resul2 = pre2*pro2;
+			int resul3 = pre3*pro3;
 			
-			// TODO: Implement Functionality Here
 			
-			Console.Write("Press any key to continue . . . ");
+			Console.WriteLine(" " +pro1+ "  " + des1 + "  " + resul1);
+			Console.WriteLine(" " +pro2+ "  " + des2 + "  " + resul2);
+			Console.WriteLine(" " +pro3+ "  " + des3 + "  " + resul3);
+		
+			
+			int subt = resul1+resul2+resul3;
+			Double iva = subt * 0.16;
+			Double total = subt + iva;
+			
+			Console.WriteLine(" Subtotal =" +subt);
+			Console.WriteLine(" Iva =" +iva);
+			Console.WriteLine(" Total =" +total);
 			Console.ReadKey(true);
+			
+			Console.WriteLine("Gracias por su preferencia!!!");
+			
 		}
 	}
 }
